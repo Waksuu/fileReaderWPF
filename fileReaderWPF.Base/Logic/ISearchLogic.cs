11358e8a84739d18ba9fa8235191ccb0ab55f969
Orwas5 <33007905+Waksuu@users.noreply.github.com>
2019-01-06 19:30:51 +0100

    Add more documentation for ISearchLogic

--- fileReaderWPF.Base/Logic/ISearchLogic.cs
+++ fileReaderWPF.Base/Logic/ISearchLogic.cs
@@ -12,6 +12,9 @@ namespace fileReaderWPF.Base.Logic
         /// <param name="extensions">IEnumerable of file extensions that we want to retrieve from folderPath</param>
         /// <param name="phrase">Sentence that we want to look for</param>
         /// <param name="folderPath">Folder in which we want to look for files (with correct extension)</param>
+        /// <exception cref="System.ArgumentNullException">Throws when one of the parameters is null, white space or extensions are empty</exception>
+        /// <exception cref="System.InvalidOperationException">Throws when directory does not exist</exception>
+        /// <exception cref="fileReaderWPF.Base.Exceptions.EmptyFolderException">Throws when directory does exist, but there are no matching files</exception>
         /// <returns>Returns IEnumerable of PhraseLocation with given parameters</returns>
         Task<IEnumerable<PhraseLocation>> SearchWordsInFilesAsync(IEnumerable<string> extensions, string phrase, string folderPath);
     }
